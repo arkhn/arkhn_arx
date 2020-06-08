b75e1731086c8147a2f5b40c0f0d2de6445c11e0
celinethi <celine@arkhn.com>
2020-06-08 19:01:42 +0200

    add example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+from arkhn_arx import Anonymizer
+
+import pandas as pd
+import json
+import os
+
+df = pd.read_csv("sample_test/data_test.csv")
+with open("sample_test/config_test.txt") as cf:
+    config_dict = json.load(cf)
+url = "http://localhost:8080"
+
+an_tool = Anonymizer(df, config_dict, url)
+
+an_df, metrics = an_tool.anonymize_dataset()
+
+os.mkdir("results")
+
+an_df.to_csv("results/anonymized_df.csv")
+metrics.to_csv("risk_metrics.csv")
