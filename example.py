c58fa02477407371fd8c9fe44cdda374cb081abd
celinethi <celine@arkhn.com>
2020-06-09 11:52:19 +0200

    change Anonymizer class init

--- example.py
+++ example.py
@@ -1,17 +1,22 @@
-from arkhn_arx import Anonymizer
+from arkhn_arx.main_arx import Anonymizer
 
 import pandas as pd
 import json
 import os
 
+# Input dataframe
 df = pd.read_csv("sample_test/data_test.csv")
+
+# Input config dict
 with open("sample_test/config_test.txt") as cf:
     config_dict = json.load(cf)
+
+# ARXaaS connection url : here ARXaaS is running locally
 url = "http://localhost:8080"
 
-an_tool = Anonymizer(df, config_dict, url)
+an_tool = Anonymizer(url)
 
-an_df, metrics = an_tool.anonymize_dataset()
+an_df, metrics = an_tool.anonymize_dataset(df, config_dict)
 
 if not os.path.exists("results"):
     os.mkdir("results")
